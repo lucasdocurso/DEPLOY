3158503710a0022afb03675f82c9f175668995a4
Lucas Machado Florão <162522619+lucasdocurso@users.noreply.github.com>
2025-09-25 21:46:36 -0300

    Add files via upload

---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const MeuComponente = () => {
+    return (
+        <div className='container'>
+            <h1>Meu Componente</h1>
+        </div>
+    )
+}
+export default MeuComponente
---
+++ src/components/MeuComponenteProp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// Componente com propriedades
+const MeuComponenteProp = ({ nome}) => {
+    return (
+        <div className='container'>
+            <h1>Olá, {nome}!</h1>
+        </div>
+    )
+}
+export default MeuComponenteProp
+    
